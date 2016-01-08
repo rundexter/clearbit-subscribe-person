9b9383a0091dac147231bb3a65f8691a999c58ce
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-08 16:58:05 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+var util = require('./util.js');
+var request = require('request').defaults({
+    baseUrl: 'https://person.clearbit.com/'
+});
+
+var pickInputs = {
+        'email': 'email',
+        'subscribe': {key: 'subscribe', type: 'boolean'}
+    },
+    pickOutputs = {
+        'id': 'person.id',
+        'name': 'person.name',
+        'email': 'person.email',
+        'gender': 'person.gender',
+        'location': 'person.location',
+        'bio': 'person.bio',
+        'site': 'person.site',
+        'employment': 'person.employment'
+    };
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +27,24 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs),
+            apiKey = dexter.environment('clearbit_api_key'),
+            api = '/v2/combined/find';
+
+        if (!apiKey)
+            return this.fail('A [clearbit_api_key] environment variable is required for this module');
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        request.get({uri: api, qs: inputs, auth: { user: apiKey, pass: '' }, json: true}, function (error, response, body) {
+            if (error)
+                this.fail(error);
+            else if (body && body.error)
+                this.fail(body.error);
+            else
+                this.complete(util.pickOutputs(body, pickOutputs));
+        }.bind(this));
     }
 };
